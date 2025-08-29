183b23ce78753d535ac91f1d8b4b251074108954
Minh Khang <khangminh530@gmail.com>
2025-08-29 10:59:22 +0700

    test https

--- src/ApiGateway.Api/Program.cs
+++ src/ApiGateway.Api/Program.cs
@@ -1,6 +1,19 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureApiService();
+
+builder.WebHost.ConfigureKestrel((options) =>
+{
+    var certPath = Environment.GetEnvironmentVariable("CERT_PATH");
+    var certPassword = Environment.GetEnvironmentVariable("CERT_PASSWORD");
+    
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps(certPath, certPassword);
+    });
+});
 
 var app = builder.Build();
 
