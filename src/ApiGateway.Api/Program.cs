c292a7ad8f1327ee0ace3debba42b00bf0ad3904
CourageCat <minhhien270103@gmail.com>
2025-09-03 12:33:57 +0700

    Update

--- src/ApiGateway.Api/Program.cs
+++ src/ApiGateway.Api/Program.cs
@@ -3,12 +3,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureApiService();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 52428800; // 50 MB
+});
 
 // builder.WebHost.ConfigureKestrel((options) =>
 // {
 //     var certPath = Environment.GetEnvironmentVariable("CERT__PATH");
 //     var certPassword = Environment.GetEnvironmentVariable("CERT__PASSWORD");
-    
+
 //     options.ListenAnyIP(5001, listenOptions =>
 //     {
 //         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
